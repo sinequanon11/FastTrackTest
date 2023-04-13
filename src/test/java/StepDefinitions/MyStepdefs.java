7d6d02dbcdb1d649dd7aa851f3cd7910f423711b
Aziz Sevi <seviseviaziz@gmail.com>
2023-04-13 18:55:54 +0300

    codes 2

---
+++ src/test/java/StepDefinitions/MyStepdefs.java
@@ -0,0 +1,9 @@
+package StepDefinitions;
+
+import io.cucumber.java.en.When;
+
+public class MyStepdefs {
+    @When("Navigate to Returning User Page")
+    public void navigateToReturningUserPage() {
+    }
+}
