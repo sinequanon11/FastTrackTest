37afa92672c0dff3a5deb323f75d0ecaa30759a9
Aziz Sevi <seviseviaziz@gmail.com>
2023-04-15 17:15:13 +0300

    codes 2

---
+++ src/test/java/StepDefinitions/LotteryTicketSteps.java
@@ -0,0 +1,9 @@
+package StepDefinitions;
+
+import io.cucumber.java.en.Then;
+
+public class LotteryTicketSteps {
+    @Then("Buy Lottery Ticket and Balance Should be Updated")
+    public void buyLotteryTicketAndBalanceShouldBeUpdated() {
+    }
+}
