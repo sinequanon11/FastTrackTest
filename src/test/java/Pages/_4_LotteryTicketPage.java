3d415dcf26fd18e32da335447454a647c1756c42
Aziz Sevi <seviseviaziz@gmail.com>
2023-04-16 13:27:37 +0300

    codes 2

---
+++ src/test/java/Pages/_4_LotteryTicketPage.java
@@ -0,0 +1,49 @@
+package Pages;
+
+import Utilities.GenWebDrv;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+
+public class _4_LotteryTicketPage extends Parent {
+
+    public _4_LotteryTicketPage() {
+
+        PageFactory.initElements( GenWebDrv.getDriver(), this );
+    }
+
+    @FindBy(xpath = "//div[@id='navigation']")
+    private WebElement hambmenu;
+
+    @FindBy(xpath = "//a[normalize-space()='Lottery']")
+    private WebElement lottery;
+
+    @FindBy(xpath = "//button[normalize-space()='Buy tickets']")
+    private WebElement buytickets;
+
+
+    WebElement myElement;
+
+    public void findAndClick(String strElement) {
+
+        switch (strElement) {
+            case "hambmenu":
+                myElement = hambmenu;
+                break;
+            case "lottery":
+                myElement = lottery;
+                break;
+            case "buytickets":
+                myElement = buytickets;
+                break;
+        }
+        clickFunction( myElement );
+    }
+
+    public void balanceUpdated(String amount1, String amount2) {
+
+        Assert.assertNotEquals( amount1, amount2, "The balance has not been updated" );
+    }
+
+}
